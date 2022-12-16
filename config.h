9bbc380fbebc7738b01077b63997350c245a3905
Simon H Moore <simon@simonhugh.xyz>
2022-12-16 00:34:22 +0000

    remove menucmd

--- config.h
+++ config.h
@@ -113,12 +113,10 @@ static const double accel_speed = 0.0;
 
 /* commands */
 static const char *termcmd[] = { TERMINAL, NULL };
-static const char *menucmd[] = { "bemenu-run", NULL };
 
 static const Key keys[] = {
 	/* Note that Shift changes certain key codes: c -> C, 2 -> at, etc. */
 	/* modifier                  key                 function        argument */
-	{ MODKEY,                    XKB_KEY_p,          spawn,          {.v = menucmd} },
 	{ MODKEY|SHIFT,              XKB_KEY_Return,     spawn,          {.v = termcmd} },
 	{ MODKEY,                    XKB_KEY_j,          focusstack,     {.i = +1} },
 	{ MODKEY,                    XKB_KEY_k,          focusstack,     {.i = -1} },
